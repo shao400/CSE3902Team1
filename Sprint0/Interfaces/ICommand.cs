bb3026e38217345216e90f49acf52ad94188e708
G-Qin <qin>
2020-05-29 13:44:46 -0400

    IPlayer added
    Author added for code reviews

--- Sprint0/Interfaces/ICommand.cs
+++ Sprint0/Interfaces/ICommand.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Sprint0.Interfaces
+//Author: Gengyi Qin
+namespace Sprint0
 {
+    
     interface ICommand
     {
         void Execute();
     }
+    
 }
---
+++ Sprint0/Interfaces/IPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+//Author: Gengyi Qin
+namespace Sprint0
+{
+    interface IPlayer
+    {
+        void Draw();
+        void Update();
+        Rectangle GetRectangle(); // use it to get the location of items etc.
+        void Right();
+        void Left();
+        void Up();
+        void Down();
+        void Attack();
+        void UseFirstItem();
+        void UseSecondItem();
+        void UseThirdItem();  //number of methods depends on number of items
+
+    }
+}
