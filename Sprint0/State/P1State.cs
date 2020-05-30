efadbd2cde6299afb7c8ab918297c2c73e9ee1d9
ChuwenSun <chuwen.sun@yahoo.com>
2020-05-29 21:47:49 -0400

    P1State added

---
+++ Sprint0/State/P1State.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0.State
+{
+    class P1State
+    {
+        private enum facing
+        {
+            up, down, left, right
+        }
+
+        private enum weapon
+        {
+            none, WoodenSword, WhiteSword, MagicalSword, MagicalRod;
+        }
+
+        private bool isAttacking;
+        private bool isTakingDmg;
+
+    }
+}
