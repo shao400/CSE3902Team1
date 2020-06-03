2d834dbe95be697ee9b6142276541a025b2d5916
Zilin Shao <66044248+shao400@users.noreply.github.com>
2020-06-03 08:06:34 +0800

    several commands class added

---
+++ Sprint0/Commands/SecondItemCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sprint0
+{
+    public class SecondItemCommand : ICommand
+    {
+        IPlayer Iplayer;
+        public SecondItemCommand(IPlayer Iplayer)
+        {
+            this.Iplayer = Iplayer;
+        }
+
+        public void Execute()
+        {
+            Iplayer.UseSecondItem();
+        }
+    }
+}
+
---
+++ Sprint0/Commands/ThirdItemCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sprint0
+{
+    public class ThirdItemCommand : ICommand
+    {
+        IPlayer Iplayer;
+        public ThirdItemCommand(IPlayer Iplayer)
+        {
+            this.Iplayer = Iplayer;
+        }
+
+        public void Execute()
+        {
+            Iplayer.UseThirdItem();
+        }
+    }
+}
+
