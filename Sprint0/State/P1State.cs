0ce51046dd28efd2f0679547fb9782d39926c198
ChuwenSun <chuwen.sun@yahoo.com>
2020-06-03 17:02:22 -0400

    P1State.update deleted

--- Sprint0/State/P1State.cs
+++ Sprint0/State/P1State.cs
@@ -38,493 +38,15 @@ namespace Sprint0.State
 
         public void Update()
         {
-            //FACING UP
-            if (this.currentFacing == facing.up)
-            {
-                //FACING UP -> STANDING
-                if (this.currentStatus == status.standing)
-                {
-                    //FACING UP -> STANDING -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
 
-                    }
-                    //FACING UP -> STANDING -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING UP -> STANDING -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING UP -> STANDING -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING UP -> STANDING -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-                //FACING UP -> ATTACKING
-                if (this.currentStatus == status.attacking)
-                {
-                    //FACING UP -> ATTACKING -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-                        //啥都不干
-                    }
-                    //FACING UP -> ATTACKING -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING UP -> ATTACKING -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING UP -> ATTACKING -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING UP -> ATTACKING -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-                //FACING UP -> WALKING
-                if (this.currentStatus == status.walking)
-                {
-                    //FACING UP -> WALKING -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-                        
-                    }
-                    //FACING UP -> WALKING -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING UP -> WALKING -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING UP -> WALKING -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING UP -> WALKING -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-                //FACING UP -> TAKINGDMG
-                if (this.currentStatus == status.takingDmg)
-                {
-                    //FACING UP -> TAKINGDMG -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-
-                    }
-                    //FACING UP -> TAKINGDMG -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING UP -> TAKINGDMG -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING UP -> TAKINGDMG -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING UP -> TAKINGDMG -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-            }
-            //FACING DOWN
-            if (this.currentFacing == facing.down)
-            {
-                //FACING DOWN -> STANDING
-                if (this.currentStatus == status.standing)
-                {
-                    //FACING DOWN -> STANDING -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-
-                    }
-                    //FACING DOWN -> STANDING -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING DOWN -> STANDING -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING DOWN -> STANDING -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING DOWN -> STANDING -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-                //FACING DOWN -> ATTACKING
-                if (this.currentStatus == status.attacking)
-                {
-                    //FACING DOWN -> ATTACKING -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-                        //啥都不干
-                    }
-                    //FACING DOWN -> ATTACKING -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING DOWN -> ATTACKING -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING DOWN -> ATTACKING -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING DOWN -> ATTACKING -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-                //FACING DOWN -> WALKING
-                if (this.currentStatus == status.walking)
-                {
-                    //FACING DOWN -> WALKING -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-
-                    }
-                    //FACING DOWN -> WALKING -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING DOWN -> WALKING -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING DOWN -> WALKING -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING DOWN -> WALKING -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-                //FACING DOWN -> TAKINGDMG
-                if (this.currentStatus == status.takingDmg)
-                {
-                    //FACING DOWN -> TAKINGDMG -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-
-                    }
-                    //FACING DOWN -> TAKINGDMG -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING DOWN -> TAKINGDMG -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING DOWN -> TAKINGDMG -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING DOWN -> TAKINGDMG -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-            }
-            //FACING LEFT
-            if (this.currentFacing == facing.left)
-            {
-                //FACING LEFT -> STANDING
-                if (this.currentStatus == status.standing)
-                {
-                    //FACING LEFT -> STANDING -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-
-                    }
-                    //FACING LEFT -> STANDING -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING LEFT -> STANDING -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING LEFT -> STANDING -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING LEFT -> STANDING -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-                //FACING LEFT -> ATTACKING
-                if (this.currentStatus == status.attacking)
-                {
-                    //FACING LEFT -> ATTACKING -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-                        //啥都不干
-                    }
-                    //FACING LEFT -> ATTACKING -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING LEFT -> ATTACKING -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING LEFT -> ATTACKING -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING LEFT -> ATTACKING -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-                //FACING LEFT -> WALKING
-                if (this.currentStatus == status.walking)
-                {
-                    //FACING LEFT -> WALKING -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-
-                    }
-                    //FACING LEFT -> WALKING -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING LEFT -> WALKING -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING LEFT -> WALKING -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING LEFT -> WALKING -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-                //FACING LEFT -> TAKINGDMG
-                if (this.currentStatus == status.takingDmg)
-                {
-                    //FACING LEFT -> TAKINGDMG -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-
-                    }
-                    //FACING LEFT -> TAKINGDMG -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING LEFT -> TAKINGDMG -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING LEFT -> TAKINGDMG -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING LEFT -> TAKINGDMG -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-            }
-            //FACING RIGHT
-            if (this.currentFacing == facing.right)
-            {
-                //FACING RIGHT -> STANDING
-                if (this.currentStatus == status.standing)
-                {
-                    //FACING RIGHT -> STANDING -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-
-                    }
-                    //FACING RIGHT -> STANDING -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING RIGHT -> STANDING -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING RIGHT -> STANDING -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING RIGHT -> STANDING -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-                //FACING RIGHT -> ATTACKING
-                if (this.currentStatus == status.attacking)
-                {
-                    //FACING RIGHT -> ATTACKING -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-                        //啥都不干
-                    }
-                    //FACING RIGHT -> ATTACKING -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING RIGHT -> ATTACKING -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING RIGHT -> ATTACKING -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING RIGHT -> ATTACKING -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-                //FACING RIGHT -> WALKING
-                if (this.currentStatus == status.walking)
-                {
-                    //FACING RIGHT -> WALKING -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-
-                    }
-                    //FACING RIGHT -> WALKING -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING RIGHT -> WALKING -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING RIGHT -> WALKING -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING RIGHT -> WALKING -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-                //FACING RIGHT -> TAKINGDMG
-                if (this.currentStatus == status.takingDmg)
-                {
-                    //FACING RIGHT -> TAKINGDMG -> NO_WEAPON
-                    if (this.currentWeapon == weapon.None)
-                    {
-
-                    }
-                    //FACING RIGHT -> TAKINGDMG -> WOODENSWORD
-                    if (this.currentWeapon == weapon.WoodenSword)
-                    {
-
-                    }
-                    //FACING RIGHT -> TAKINGDMG -> WHITESWORD
-                    if (this.currentWeapon == weapon.WhiteSword)
-                    {
-
-                    }
-                    //FACING RIGHT -> TAKINGDMG -> MAGICALSWORD
-                    if (this.currentWeapon == weapon.MagicalSword)
-                    {
-
-                    }
-                    //FACING RIGHT -> TAKINGDMG -> MAGICALROD
-                    if (this.currentWeapon == weapon.MagicalRod)
-                    {
-
-                    }
-                }
-            }
         }
 
         public void right()
         {
             currentStatus = status.walking;
             currentFacing = facing.right;
-            if (currentWeapon == weapon.None)
+            currentSprite = new LinkNoneMovingRightSprite();
+/*            if (currentWeapon == weapon.None)
             {
                 currentSprite = new LinkNoneMovingRightSprite();
             }
@@ -543,14 +65,15 @@ namespace Sprint0.State
             else if (currentWeapon == weapon.MagicalRod)
             {
                 //currentSprite = new LinkMagicalMovingRightSprite();
-            }
+            }*/
         }
 
         public void left()
         {
             currentStatus = status.walking;
             currentFacing = facing.left;
-            if (currentWeapon == weapon.None)
+            currentSprite = new LinkNoneMovingLeftSprite();
+/*            if (currentWeapon == weapon.None)
             {
                 currentSprite = new LinkNoneMovingLeftSprite();
             }
@@ -569,14 +92,15 @@ namespace Sprint0.State
             else if (currentWeapon == weapon.MagicalRod)
             {
                 //currentSprite = new LinkMagicalRMovingLeftSprite();
-            }
+            }*/
         }
 
         public void Up()
         {
             currentStatus = status.walking;
             currentFacing = facing.up;
-            if (currentWeapon == weapon.None)
+            currentSprite = new LinkNoneMovingUpSprite();
+            /*if (currentWeapon == weapon.None)
             {
                 currentSprite = new LinkNoneMovingUpSprite();
             }
@@ -595,7 +119,7 @@ namespace Sprint0.State
             else if (currentWeapon == weapon.MagicalRod)
             {
                 //currentSprite = new LinkMagicalRMovingUpSprite();
-            }
+            }*/
         }
         public void Stand()
         {
@@ -624,7 +148,8 @@ namespace Sprint0.State
         {
             currentStatus = status.walking;
             currentFacing = facing.down;
-            if (currentWeapon == weapon.None)
+            currentSprite = new LinkNoneMovingDownSprite();
+/*            if (currentWeapon == weapon.None)
             {
                 currentSprite = new LinkNoneMovingDownSprite();
             }
@@ -643,7 +168,7 @@ namespace Sprint0.State
             else if (currentWeapon == weapon.MagicalRod)
             {
                 //currentSprite = new LinkMagicalRMovingDownSprite();
-            }
+            }*/
         }
 
         public void UseFirstItem()
