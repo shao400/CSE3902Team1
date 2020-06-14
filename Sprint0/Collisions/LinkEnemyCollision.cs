fa7ee336b7c50d2036a9fc5c335c3c333b39e58d
G-Qin <qin>
2020-06-14 17:03:01 -0400

    LinkEnemyCollision added

---
+++ Sprint0/Collisions/LinkEnemyCollision.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sprint0.Player;
+
+namespace Sprint0.Collisions
+{
+    class LinkEnemyCollision
+    {
+        private Player1 myPlayer;
+
+        public LinkEnemyCollision(Player1 player)
+        {
+            myPlayer = player;
+        }
+
+        public void CollisionTest(Player1 player, List<Enemy> enemies)
+        {
+
+        }
+    }
+}
