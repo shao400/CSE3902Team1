c0b435028563a7c4d75002e29fa8b28ca00f476a
G-Qin <qin>
2020-06-15 12:59:28 -0400

    IEnemy added, LinkEnemyCollision updated

--- Sprint0/Collisions/LinkEnemyCollision.cs
+++ Sprint0/Collisions/LinkEnemyCollision.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Sprint0.Interfaces;
 using Sprint0.Player;
 
 namespace Sprint0.Collisions
@@ -16,9 +18,40 @@ namespace Sprint0.Collisions
             myPlayer = player;
         }
 
-        public void CollisionTest(Player1 player, List<Enemy> enemies)
+        public int CollisionTest(Player1 player, List<IEnemy> enemies)
         {
+            Rectangle linkRectangle = myPlayer.GetRectangle();
+            Rectangle enemyRectangle;
+            Rectangle intersectionRectangle;
+            bool enemyKilled = false;
+            Queue<IEnemy> deadEnemies = new Queue<IEnemy>();
 
+            foreach (IEnemy enemy in enemies)
+            {
+                enemyRectangle = enemy.GetRectangle();
+                intersectionRectangle = Rectangle.Intersect(linkRectangle, enemyRectangle);
+
+                if (!intersectionRectangle.IsEmpty)
+                {
+                    if (intersectionRectangle.Width >= intersectionRectangle.Height)
+                    {
+                        //myPlayer.Bounce(); need implementation
+                        enemyKilled = true;
+                        deadEnemies.Enqueue(enemy);
+                    }
+                    else
+                    {
+                        //myPlayer.Hit(); need implementation
+                    }
+                }
+            }
+
+            while (deadEnemies.Count > 0)
+            {
+                IEnemy enemy = deadEnemies.Dequeue();
+                enemies.Remove(enemy);
+            }
+            return 0;
         }
     }
 }
---
+++ Sprint0/Interfaces/IEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Sprint0.Interfaces
+{
+    interface IEnemy
+    {
+        void Draw();
+        void Fall(/*to be determined*/);
+        void Reverse(/*to be determined*/);
+        void Update(/*to be determined*/);
+        Rectangle GetRectangle();
+    }
+}
