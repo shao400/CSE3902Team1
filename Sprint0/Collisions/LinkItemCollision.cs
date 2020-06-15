3ecdf55677584949b0240796f834daa43605555c
ZinaZhang <zina20001126@gmail.com>
2020-06-15 14:13:47 -0400

    item and link collision

---
+++ Sprint0/Collisions/LinkItemCollision.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Sprint0.Interfaces;
+using Sprint0.Player;
+
+namespace Sprint0.Collisions
+{
+    class LinkItemCollision
+    {
+        private Player1 myPlayer;
+
+        public LinkItemCollision(Player1 player)
+        {
+            myPlayer = player;
+        }
+        public void CollisionItem(Player1 player, List<IItem> items)
+        {
+            Rectangle linkRectangle = myPlayer.GetRectangle();
+            Rectangle itemRectangle;
+            Rectangle intersectionRectangle;
+            List<IItem> holdItems=new List<IItem>();
+            foreach (IItem item in items)
+            {
+                itemRectangle = item.GetRectangle();
+                intersectionRectangle = Rectangle.Intersect(linkRectangle, itemRectangle);
+
+                if (!intersectionRectangle.IsEmpty)
+                {
+                    holdItems.Add(item);
+                }
+            }
+        }
+    }
+}
+ 
+
+
