4b8810fa4f6b9306bf81bbd39468749fd30d3a8c
hzz.Github <55898477+HzzCoding@users.noreply.github.com>
2020-06-15 16:37:58 -0400

    LinkBlock_Collison

---
+++ Sprint0/Collisions/LinkBlockCollision.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Sprint0.Interfaces;
+using Sprint0.Player;
+
+namespace Sprint0.Collisions
+{
+    class LinkBlockCollision
+    {
+        private Player1 myPlayer;
+
+        public LinkBlockCollision(Player1 player)
+        {
+            myPlayer = player;
+        }
+
+
+        public void CollisionItem(Player1 player, List<IBlock> blocks)
+        {
+            Rectangle linkRectangle = myPlayer.GetRectangle();
+            Rectangle blockRectangle;
+            Rectangle intersectionRectangle;
+
+            foreach (IBlock block in blocks)
+            {
+                blockRectangle = block.GetRectangle();             
+                intersectionRectangle = Rectangle.Intersect(linkRectangle, blockRectangle);
+
+                if (!intersectionRectangle.IsEmpty)
+                {
+                    // check the collison occuring direction
+                    if ((intersectionRectangle.Width >= intersectionRectangle.Height)) // from up or down
+                    {
+                        if (linkRectangle.Y > blockRectangle.Y) // from down
+                        {
+                            linkRectangle.Y += intersectionRectangle.Height;
+                        }
+                        else //from up
+                        {
+                            linkRectangle.Y -= intersectionRectangle.Height;
+                        }
+                    }
+                    else //from right or left
+                    {
+                        if (linkRectangle.X > blockRectangle.X)//from right
+                        {
+                            linkRectangle.X += intersectionRectangle.Width;
+                        }
+                        else //from left
+                        {
+                            linkRectangle.X -= intersectionRectangle.Width;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+ 
+
+
---
+++ Sprint0/Collisions/temp.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamYoshi.Collisions
+{
+    public class MarioBlockCollision
+    {
+
+        private Mario myMario;
+
+        public MarioBlockCollision(Mario mario)
+        {
+            myMario = mario;
+        }
+
+        public Tuple<int, int, bool, bool> BlockCollisionTest(SoundEffects sound, HUD hud, IList<IBlock> blocks, int x, int y, IList<IItem> items, Camera c)
+        {
+            Rectangle marioRectangle = myMario.GetRectangle();
+            Rectangle blockRectangle;
+            Rectangle intersectionRectangle;
+
+
+
+            bool hitGround = false; // x
+            bool hitCeiling = false; // x
+
+            Tuple<int, int, bool, bool> result;
+            foreach (IBlock block in blocks)
+            {
+
+
+
+                blockRectangle = block.GetRectangle();
+                intersectionRectangle = Rectangle.Intersect(marioRectangle, blockRectangle);
+                if (!intersectionRectangle.IsEmpty)
+                {
+
+
+                    if ((intersectionRectangle.Width >= intersectionRectangle.Height))
+                    {
+                        if (marioRectangle.Y > blockRectangle.Y)
+                        {
+                            block.Hit(items, true, myMario.IsLarge(), hud, sound);
+                            hitCeiling = true;
+                            y += (intersectionRectangle.Height);
+                            marioRectangle.Y += intersectionRectangle.Height;
+                        }
+                        else
+                        {
+                            hitGround = true;
+                            if (block is Blocks.VerticalWarpPipe)
+                            {
+                                if (myMario.IsCrouching())
+                                {
+                                    c.SetXPos(Constants.verticalWarp.Item1 - Constants.tileLength);
+                                    x = Constants.verticalWarp.Item1;
+                                    y = Constants.verticalWarp.Item2;
+                                    break;
+                                }
+                            }
+                            y -= (intersectionRectangle.Height);
+                            marioRectangle.Y -= intersectionRectangle.Height;
+                        }
+                    }
+                    else
+                    {
+                        if (marioRectangle.X > blockRectangle.X)
+                        {
+                            x += (intersectionRectangle.Width);
+                            marioRectangle.X += intersectionRectangle.X;
+                        }
+                        else
+                        {
+                            x -= (intersectionRectangle.Width);
+                            marioRectangle.X -= intersectionRectangle.X;
+                            if (block is Blocks.HorizontalWarpPipe)
+                            {
+                                c.SetXPos(Constants.horizontalWarp.Item1 - Constants.tileLength);
+                                x = Constants.horizontalWarp.Item1;
+                                y = Constants.horizontalWarp.Item2;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            result = new Tuple<int, int, bool, bool>(x, y, hitCeiling, hitGround);
+            return result;
+        }
+
+    }
+}
