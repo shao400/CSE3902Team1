b2f2fda4d92bb91588369189360549ab2d5439bd
LufeiOy <66043733+LufeiOy@users.noreply.github.com>
2020-06-15 17:07:50 -0400

    add itemcollision

---
+++ Sprint0/Collisions/AllItemCollision.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Sprint0.Interfaces;
+
+
+namespace Sprint0.Collisions
+{
+    class AllItemCollision
+    {
+        private IItem thisItem;
+
+        public AllItemCollision(IItem item)
+        {
+            thisItem = item;
+        }
+        public void AllItemCollisionTest()
+        {
+            Rectangle thisRectangle = thisItem.GetRectangle(IList < IBlock > blocks);
+            Rectangle blockRectangle;
+            Rectangle intersectionRectangle;
+            foreach (IBlock block in blocks)
+            {
+
+                blockRectangle = block.GetRectangle();
+                intersectionRectangle = Rectangle.Intersect(thisRectangle, blockRectangle);
+
+                if (!intersectionRectangle.IsEmpty)
+                {
+                    if (intersectionRectangle.Width >= intersectionRectangle.Height)
+                    {
+
+                    }
+
+                    else if (thisRectangle.X < blockRectangle.X)
+                    {
+
+                    }
+                    else if (thisRectangle.X > blockRectangle.X)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
