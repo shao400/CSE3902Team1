484fb4e2c0339da69bafd0aa6cd70a840a2fc99f
ZinaZhang <zina20001126@gmail.com>
2020-07-04 12:06:43 -0400

    little edit to sprite

---
+++ Sprint0/Sprite/woodenSword.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Sprint0.Sprite
+{
+    public class woodenSwordSprite : ISprite
+    {
+        //1,154,7,16 (whole)
+        //1,154,7,13 (with green part)
+        //3,154,3,11 (only sword)
+        //final position link + (5,11) / (11,4) / (3,-12)
+        private static SpriteBatch mySpriteBatch;
+        private static Texture2D myTexture;
+        Color myColor = Color.CornflowerBlue;
+        int frame = 0;
+        Rectangle destinationRec;
+        Rectangle sourceRec = new Rectangle(1, 54, 7, 16);
+
+
+        public void Update()
+        {
+            
+        }
+
+
+        public void LoadContent(SpriteBatch batch, Texture2D f)
+        {
+
+            mySpriteBatch = batch;
+            myTexture = f;
+
+        }
+
+        public void Draw(Vector2 location, Boolean isDamaged)
+        {
+            frame++;
+            if (frame == 12)
+            {
+                frame = 0;
+            }
+            if (frame < 9)
+            {
+                destinationRec = new Rectangle((int)location.X+5, (int)location.Y+12, 45, 45);
+            }
+            else
+            {
+                destinationRec = new Rectangle((int)location.X, (int)location.Y, 45, 45);
+            }
+            mySpriteBatch.Begin();
+            mySpriteBatch.Draw(myTexture, destinationRec, sourceRec, Color.White);
+            mySpriteBatch.End();
+
+        }
+
+
+    }
+}
+
